af34da1db10115f686c6ebd7f1dd23deac2d7a50
pablomgProgOO <143107479+pablomgProgOO@users.noreply.github.com>
2025-03-06 15:41:04 -0500

    k

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/appweb-trp01/",
+  base: "/appweb-trpr01/",
   plugins: [vue()],
 })
